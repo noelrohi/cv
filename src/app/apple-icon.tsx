7414fe2fd7a3c097500b852dd8e559532356a0fd
gneiru <noelrohi59@gmail.com>
2023-12-27 19:21:17 +0800

    icons

---
+++ src/app/apple-icon.tsx
@@ -0,0 +1,12 @@
+import Icon from "./icon";
+
+export const runtime = "edge";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const contentType = "image/png";
+
+export default Icon;
