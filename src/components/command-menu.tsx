ade88313489bf2ee8ecdae70f2e01126f77b29f7
gneiru <noelrohi59@gmail.com>
2024-01-06 09:16:47 +0800

    Add button for command menu for small screens

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -12,6 +12,8 @@ import {
   CommandSeparator,
 } from "@/components/ui/command";
 import { isMacOs } from "@/lib/utils";
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
 
 interface Props {
   links: { url: string; title: string }[];
@@ -35,7 +37,7 @@ export const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
-      <p className="fixed bottom-0 left-0 right-0 border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden">
+      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden xl:block">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">{isMacOs() && "⌘"}</span>
@@ -43,6 +45,14 @@ export const CommandMenu = ({ links }: Props) => {
         </kbd>{" "}
         to open the command menu
       </p>
+      <Button
+        onClick={() => setOpen((open) => !open)}
+        variant="outline"
+        size="icon"
+        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl print:hidden xl:hidden"
+      >
+        <Menu className="my-6 size-6" />
+      </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
         <CommandInput placeholder="Type a command or search..." />
         <CommandList>
