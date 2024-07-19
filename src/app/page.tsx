c1b5c6bb57d2cc6cf53131447dbe0185c6901dd5
gneiru <noelrohi59@gmail.com>
2024-07-19 11:50:02 +0800

    fix: biome script, sort classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,7 +150,7 @@ export default function Page() {
 
         <Section className="print-force-new-page scroll-mb-16">
           <h2 className="font-bold text-xl">Projects</h2>
-          <div className="-mx-3 grid grid-cols-1 gap-3 lg:grid-cols-3 md:grid-cols-2 print:grid-cols-3 print:gap-2">
+          <div className="-mx-3 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-3 print:gap-2">
             {RESUME_DATA.projects.map((project) => {
               return (
                 <ProjectCard
